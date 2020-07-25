3b21bd668092a9397c08325a6f2dda58b50651b4
Chogrammer26 <48506056+Chogrammer26@users.noreply.github.com>
2020-07-25 20:07:53 +0800

    Update segIntersect.cpp

--- segIntersect.cpp
+++ segIntersect.cpp
@@ -18,6 +18,7 @@ class lineSeg {
     }
 }; 
 
+
 bool isSegIntersect(lineSeg &l1, lineSeg&l2); 
 
 int main()
@@ -26,7 +27,15 @@ int main()
     lineSeg l2; 
     
     l1.setPoints(4,6,7,8);
-    l2.setPoints(5,7,8,9);     
+    l2.setPoints(5,7,8,9);
+    
+    std::cout << "Details of Line Segment 1 : \n"
+              << "End Point 1 : (" << l1.p1.x << ',' << l1.p1.y << ") "
+              << "End Point 2 : (" << l1.p2.x << ',' << l1.p2.y << ")\n";
+    
+    std::cout << "Details of Line Segment 2 : \n"
+              << "End Point 1 : (" << l2.p1.x << ',' << l2.p1.y << ") "
+              << "End Point 2 : (" << l2.p2.x << ',' << l2.p2.y << ")"; 
 }
 
 bool isSegIntersect(lineSeg &l1, lineSeg&l2) 
